2a217988e2ec062356871850710f0fe209a4a924
VitaliiZadniprianets <vzadnepr@ukr.net>
2023-03-17 22:42:23 +0200

    first try

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+let force = 17;
+let weight = 6;
+let acceleration = force / weight;
+let spaceBeetween = "<br />";
+    document.write('***************' + spaceBeetween + spaceBeetween + 
+    'Ускорение тела при силе F = '+ force + ' и массе m = ' + weight + ':' 
+    + spaceBeetween + spaceBeetween + 
+    '--------------------' + spaceBeetween + spaceBeetween +  
+    'a = '+ acceleration + spaceBeetween + spaceBeetween + 
+    '--------------------' + spaceBeetween + spaceBeetween + 
+    `end.`);
