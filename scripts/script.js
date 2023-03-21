bd2725748dc2bfa2d6b6577734cbb0c894d4e38a
VitaliiZadniprianets <vzadnepr@ukr.net>
2023-03-21 15:55:13 +0200

    Homework2 end

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,23 @@
-let force = 17;
-let weight = 6;
-let acceleration = force / weight;
-let spaceBeetween = "<br />";
-    document.write('***************' + spaceBeetween + spaceBeetween + 
-    'Ускорение тела при силе F = '+ force + ' и массе m = ' + weight + ':' 
-    + spaceBeetween + spaceBeetween + 
-    '--------------------' + spaceBeetween + spaceBeetween +  
-    'a = '+ acceleration + spaceBeetween + spaceBeetween + 
-    '--------------------' + spaceBeetween + spaceBeetween + 
-    `end.`);
+// Отримання анкетних данних від користувачів
+let userFirst = prompt("Введіть анектні дані про першого студента у такому порядку : xx років ім'я та прізвище студента");
+let userSecond = prompt("Введіть анектні дані про другого студента у такому порядку : xx років ім'я та прізвище студента");
+let userThird =  prompt("Введіть анектні дані про третього студента у такому порядку : xx років ім'я та прізвище студента");
+
+// Обчислення середнього віку
+const averageAge = (parseInt(userFirst) + parseInt(userSecond) + parseInt(userThird)) / 3;
+
+// для зручності  створив 
+spaceBetween = '\n' + '\n';
+
+// консоль
+console.log('**************' ,spaceBetween,
+'Список студентів, що зареєструвалися на вебінар :',spaceBetween, 
+'1.' ,userFirst, spaceBetween,
+'2.' ,userSecond, spaceBetween,
+'3.' ,userThird, spaceBetween,
+'--------------' ,spaceBetween,
+'Середній вік студента :',averageAge, spaceBetween,
+'--------------' ,spaceBetween,
+'**************' );
+
+
