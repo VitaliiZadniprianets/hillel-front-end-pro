0abb683e6207d2394288f3466c3f9537351a6f40
Vitalii Zadniprianets <vzadnepr@ukr.net>
2023-03-22 22:31:06 +0200

    end

--- scripts/script.js
+++ scripts/script.js
@@ -7,9 +7,9 @@ let age = prompt("Please enter your age:");
 (login && password) && alert("Welcome");
 (login === '' || password === '') && alert("Login or password incorrect"); 
 
-(login === '' && password === '') && alert("Login and password are required!"); 
-(login === '' && password) && alert("Login required!"); 
-(login && password === '') && alert("Password are required!"); 
+(login === '' && password === '') && alert("Login and password are required"); 
+(login === '' && password) && alert("Login required"); 
+(login && password === '') && alert("Password are required"); 
 (login && password) && alert("Welcome");
 
 alert((age >= 18 && age <= 28 && 'Hello') || 'Goodbay');
