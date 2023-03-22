33f800b89765d12f931d94d2dc200a8e41ed2408
VitaliiZadniprianets <vzadnepr@ukr.net>
2023-03-22 12:06:30 +0200

    part 1

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,5 @@
-// Отримання анкетних данних від користувачів
-let userFirst = prompt("Введіть анектні дані про першого студента у такому порядку : xx років ім'я та прізвище студента");
-let userSecond = prompt("Введіть анектні дані про другого студента у такому порядку : xx років ім'я та прізвище студента");
-let userThird =  prompt("Введіть анектні дані про третього студента у такому порядку : xx років ім'я та прізвище студента");
+let userName = prompt("Please enter your name:")  || "anonym";
 
-// Обчислення середнього віку
-const averageAge = (parseInt(userFirst) + parseInt(userSecond) + parseInt(userThird)) / 3;
+let greeting = (userName === "admin" && "Hello admin") || "Welcome";
 
-// для зручності  створив 
-spaceBetween = '\n' + '\n';
-
-// консоль
-console.log('**************' ,spaceBetween,
-'Список студентів, що зареєструвалися на вебінар :',spaceBetween, 
-'1.' ,userFirst, spaceBetween,
-'2.' ,userSecond, spaceBetween,
-'3.' ,userThird, spaceBetween,
-'--------------' ,spaceBetween,
-'Середній вік студента :',averageAge, spaceBetween,
-'--------------' ,spaceBetween,
-'**************' );
-
-
+alert(greeting);
