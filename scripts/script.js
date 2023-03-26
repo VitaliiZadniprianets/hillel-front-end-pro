cf7b0452d1e1e2aefe674f52d82d9d47ad876122
Vitalii Zadniprianets <vzadnepr@ukr.net>
2023-03-26 15:42:47 +0300

    first

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+const space = '<br>';
+
+/* 1. Вивести на сторінку в один рядок через кому числа від 10 до 20. */
+document.write('1: Вивести на сторінку в один рядок через кому числа від 10 до 20.' + space);
+ const maxNumber1 = 20 ;
+for (let i = 10; i<=maxNumber1; i++) {
+if (i<maxNumber1) {
+    document.write(i + ', ')
+}
+else {
+    document.write(i + '.' )
+}
+} ;
+
+/* Вивести квадрати чисел від 10 до 20. */
+document.write(space + space + '2: Вивести квадрати чисел від 10 до 20.' + space);
