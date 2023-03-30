adbebafe1a5adb061851d7e15deedb8bb6f9a2ba
Vitalii Zadniprianets <vzadnepr@ukr.net>
2023-03-30 15:38:41 +0300

    step 1

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,55 @@
 let n = parseInt(prompt('Введіть  розмір(length) масиву '));
 
-while (isNaN(n) || n < 1) {
-  n = parseInt(prompt("Введено некорректне значення, спробуйте ввести валідне значення."));
+if (n !== null) {
+
+  while (isNaN(n) || n < 1) {
+    n = parseInt(prompt("Введено некорректне значення, спробуйте ввести валідне значення."));
+  };
 };
 
 console.log ("array length " + n) ;
 
-const arr_1 = [] ;
+const A = [];
+  
+// Определяем мин //
+let minVal = parseInt(prompt('Введіть мінімальне значення для елементів масиву'));
 
-for (let i = 0; i < n ; i++) {
-  let val_1 = parseInt(prompt('Введіть значення для елементів масиву'));
+if (minVal !== null) { 
 
-  while (isNaN(val_1) || val_1 < 0) {
-    val_1 = parseInt(prompt("Введено некорректне значення, спробуйте ввести валідне значення."));
-  }
-
-  arr_1[i] = val_1 ;
+  while (isNaN(minVal)) {
+    minVal = parseInt(prompt("Введено некорректне значення, спробуйте ввести валідне значення."));
+  };
 };
+ 
+console.log("Мінімальне значення для елементів масиву з іменем A = " + minVal );
 
-console.log (arr_1) ;
+minVal = +minVal;
 
+// Определяем макс //
+
+let maxVal = parseInt(prompt('Введіть максимальне значення для елементів масиву'));
+
+if (maxVal !== null) { 
+
+  while (isNaN(maxVal)) {
+    minVal = parseInt(prompt("Введено некорректне значення, спробуйте ввести валідне значення."));
+  };
+};
+ 
+console.log("Максимальне значення для елементів масиву з іменем A = " + maxVal );
+
+maxVal = +maxVal;
+
+// Избежания ошибки юзера , когда мин больше макс //
+
+if (maxVal < minVal ) {
+  let temp = maxVal ;
+  maxVal = minVal ;
+  minVal = temp ;
+
+  alert ("Мінімальне значення не може бути більше максимального, тому ваші значення приведені у відповідність з умовами")
+
+  console.log("Мінімальне значення для елементів масиву з іменем A = " + minVal );
+  console.log("Максимальне значення для елементів масиву з іменем A = " + maxVal );
+};
 
