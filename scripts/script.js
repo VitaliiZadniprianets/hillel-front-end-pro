f32c8d64c40476c6117d2b87f58f5c9a353686f7
Vitalii Zadniprianets <vzadnepr@ukr.net>
2023-04-02 14:16:53 +0300

    end task

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,58 @@
-const minNumber = 8;
-const maxNumber = 15;
+const minNumber = 5;
+const maxNumber = 20;
 
-const row = 5;
-const column = 5;
+const row = 6;
+const column = 6;
 
-const A = [];
-A.length = row ;
+const matrix = [];
+matrix.length = row ;
 
-let maxSum;
-let maxSumIndex;
-let minSum;
-let minSumIndex;
+console.log(matrix);
+
+let maxSumm;
+let maxSummIndex;
+
+let minSumm;
+let minSummIndex;
 
 for (let i = 0; i < row; i++) {
-  A[i] = new Array(column);
+  matrix[i] = new Array(column);
 
   for (let j = 0; j < column; j++) {
-    A[i][j] = Math.round(Math.random() * (maxNumber - minNumber) + minNumber);
+    matrix[i][j] = Math.round(Math.random() * (maxNumber - minNumber) + minNumber);
   };
 };
 
-console.log(A);
+console.log(matrix);
 
+for (let i = 0; i < column; i++) {
+  let summ = 0;
+  
+  for (let j = 0; j < row; j++) {
+    summ += matrix[j][i];
+  };
 
+  console.log(`Сума елементів у стовпці з індексом [${i}] = ${summ}`);
 
+  if (i === 0) {
+    maxSumm = summ;
+    maxSummIndex = i;
 
+    minSumm = summ;
+    minSummIndex = i;
+    continue;
+  };
 
+  if (summ > maxSumm) {
+    maxSumm = summ;
+    maxSummIndex = i;
+  };
+
+  if (summ < minSumm) {
+    minSumm = summ;
+    minSummIndex = i;
+  };
+};
+
+console.log(`У стовпці з індексом [${minSummIndex}] найменша сума елементів = ${minSumm}`);
+console.log(`У стовпці з індексом [${maxSummIndex}] найбільша сума елементів = ${maxSumm}`);
