442a2957967e675e7949045e05b65729a0ab6cc8
Vitalii Zadniprianets <vzadnepr@ukr.net>
2023-04-06 17:17:48 +0300

    3 task

--- scripts/script.js
+++ scripts/script.js
@@ -1,56 +1,21 @@
-///// 1 задание ////
+////// 3 задание ///////
 
-const B = [ 80, 75, true, undefined, NaN, false, null, 'dfanjdfhkadk', 100, 5];
-console.log(B);
+const countRows = +prompt('Введіть кількість рядків для двомірного массиву');
+const countColumns = +prompt('Введіть кількість колонок для двомірного массиву');
 
-function averageOfNumberMassive (array) {
-  let sum = 0;
-  let count = 0;
+function createMatrix(rows, columns) {
+  let matrix = [];
 
-  for (let i = 0; i < array.length; i++) {
-    if (typeof array[i] === 'number' && !isNaN(array[i])) {
-      sum += array[i];
-      count++;
+  for(let i = 0; i < rows; i++) {
+    let row = [];
+    for(let j = 0; j < columns; j++) {
+      let val = prompt(`Введіть значення для елемента [${i}][${j}]`);
+      row.push(val);
     };
-  };
-  return sum / count
-};
-
-console.log('Average Of Numbers: ' + averageOfNumberMassive(B));
-
-//////// 2 задание////
-
-const first = +prompt ('Введіть перше число');
-const mathOperation = prompt ('Оберіть одну з  дій: +, -, *, /, %, ^.');
-const second = +prompt ('Введіть друге число');
-
-function doMath(x, znak, y) {
-  let result;
-  
-  switch(znak) {
-    case '+':
-      result = x + y;
-      break;
-    case '-':
-      result = x - y;
-      break;
-    case '*':
-      result = x * y;
-      break;
-    case '/':
-      result = x / y;
-      break;
-    case '%':
-      result = x % y;
-      break;
-    case '^':
-      result = Math.pow(x, y);
-      break;
-    default:
-      return 'Непідтримуваний знак операції!';
+    matrix.push(row);
   };
 
-  return result;
+  return matrix;
 };
 
-console.log(doMath(first,mathOperation,second));
+console.log(createMatrix(countRows, countColumns));
