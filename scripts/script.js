ebed3a17c7a654f05874d70a3e295292d41a6f99
Vitalii Zadniprianets <vzadnepr@ukr.net>
2023-04-12 16:37:57 +0300

    end

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+const arr = [[1,2,3, [3.1]], 4, [5,6, [7, 8]]];
+
+function flat(mass) {
+  let modArr = [];
+
+  for (let i = 0; i < mass.length; i++) {
+    if (Array.isArray(mass[i])) {
+        modArr = modArr.concat(flat(mass[i]));
+    } else {
+        modArr.push(mass[i]);
+    };
+  };
+  return modArr;
+};
+
+console.log(flat(arr));
