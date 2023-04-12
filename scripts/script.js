cb4c41a6ab13ad643c6d3e45ec78abeaabb820a8
Vitalii Zadniprianets <vzadnepr@ukr.net>
2023-04-12 16:00:13 +0300

    end

---
+++ scripts/script.js
@@ -0,0 +1,37 @@
+///// Задача  №1 /////
+
+function reverseSomeRow (n) {
+if (n <= 0) {
+    return;
+} else {
+console.log(n);
+return reverseSomeRow(n - 1);
+  };
+};
+
+reverseSomeRow(5);
+console.log("--------------------------");
+
+///// Задача №2 //////
+
+function sumToArray (arr) {
+  if (arr.length === 1 ) {
+    return arr[0]; 
+  };
+  const sum = arr[0] + sumToArray(arr.slice(1));
+  return sum;
+};
+
+console.log (sumToArray([1, 2, 3]));
+console.log("--------------------------");
+
+////// Задача №3 ///////
+
+function factorial (n) {
+    if (n === 0 ) {
+      return 1 ;
+    };
+    return n * factorial(n - 1) ;
+};
+
+console.log(factorial(5));
