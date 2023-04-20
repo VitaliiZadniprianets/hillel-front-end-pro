0c2142890476ec15eabc710b49920ec982f170ca
Vitalii Zadniprianets <vzadnepr@ukr.net>
2023-04-20 17:33:23 +0300

    end task

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,26 @@
 const obj = {
   result: '',
-  copy: function (buffer) {
-    this [buffer] = this.result;
-    return this;
+  copy: function(buffer) {
+      this[buffer] = this.result;
+      return this;
   },
   clear: function() {
-    this.result = 0;
-    return this;
+      this.result = 0;
+      return this;
   },
-  doFunction: function (func,x,y) {
-    this.result = func(x,y);
-    return this;
+  doFunction: function(func, x, y) {
+      this.result = func(x, y)
+      return this;
   },
-  target: function (property) {
-    let res = this.result;
-    this.clear();
-    this[property] = res;
+  target: function(property) {
+    this.doFunction = function(func, x, y) {
+      this[property] = func(x, y);
+      return this;
+    };
+    this.clear = function() {
+      this[property] = 0;
+      return this;
+    };
     return this;
   },
 };
@@ -28,10 +33,7 @@ function mul(x, y) {
   return x * y;
 };
 
-obj .doFunction(sum, 2, 4) .copy('nom_name') .target('summary') .doFunction(sum, 2, 4)
-obj.summary
-obj.clear()
-obj.summary
+obj.doFunction(mul, 4, 6).copy('nom_name').clear().target('summary').doFunction(sum, 2, 4);
 console.log(obj.result);
 console.log(obj.nom_name);
 console.log(obj.summary);
