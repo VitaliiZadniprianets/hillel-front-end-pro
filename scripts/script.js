09f392cc1751c588aebd3daad626c694865afc57
Vitalii Zadniprianets <vzadnepr@ukr.net>
2023-04-20 14:35:18 +0300

    end

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,4 @@
-//////// Задача № 1 /////////
-
-const obj = {
-  p: 600,
-  str: 'hello',
-  y: -50,
-  renderObject: renderObject,
-};
-
-function renderObject() {
-  let result = '';
-  let rowBreak = '<br>';
-  for(let key in this) {
-    if (key !== 'renderObject' ) {
-    result += key + ': ' + this[key] + rowBreak;
-    };
-  };
-  document.write(result);
-};
-
-obj.renderObject();
-
-//////// Задача № 2 /////////
-
 const data = {
-  addRecord: addRecord,
   p: 600,
   str: 'hello',
   y: -50,
@@ -37,7 +12,7 @@ function addRecord() {
   };
 };
 
-data.addRecord({ x: 10 }, { y: 20 }, { z: 30, x: 50 });
+addRecord.call(data, {x: 10}, {y: 20}, {z: 30, x: 50});
 
 console.log(data.x); // 50
 console.log(data.y); // 20
@@ -45,3 +20,21 @@ console.log(data.z); // 30
 console.log(data.p); // 600
 console.log(data.str); // 'hello'
 console.log(data);
+
+/////////////////////////////////////////////////
+console.log('---------------------------------');
+
+const data1 = {
+  p: 600,
+  str: 'hello',
+  y: -50,
+};
+
+addRecord.apply(data1, [{x: 10}, {y: 20}, {z: 30, x: 50}]);
+
+console.log(data1.x); // 50
+console.log(data1.y); // 20
+console.log(data1.z); // 30
+console.log(data1.p); // 600
+console.log(data1.str); // 'hello'
+console.log(data1);
