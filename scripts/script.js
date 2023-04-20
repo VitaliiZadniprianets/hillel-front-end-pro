29eea677430d1152f60448665334295bc0a4af80
Vitalii Zadniprianets <vzadnepr@ukr.net>
2023-04-20 16:02:55 +0300

    end task

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,38 @@
 const obj = {
   result: '',
-  copy(buffer) {
+  copy: function (buffer) {
     this [buffer] = this.result;
     return this;
   },
-  clear function() {
-    
-  }
-}
+  clear: function() {
+    this.result = 0;
+    return this;
+  },
+  doFunction: function (func,x,y) {
+    this.result = func(x,y);
+    return this;
+  },
+  target: function (property) {
+    let res = this.result;
+    this.clear();
+    this[property] = res;
+    return this;
+  },
+};
+
+function sum(x, y) {
+  return x + y;
+};
+
+function mul(x, y) {
+  return x * y;
+};
+
+obj .doFunction(sum, 2, 4) .copy('nom_name') .target('summary') .doFunction(sum, 2, 4)
+obj.summary
+obj.clear()
+obj.summary
+console.log(obj.result);
+console.log(obj.nom_name);
+console.log(obj.summary);
+console.log(obj);
