daec8a368f48f5e7a1053cd104fc8979daf49d63
Vitalii Zadniprianets <vzadnepr@ukr.net>
2023-04-27 14:21:29 +0300

    end task

--- scripts/script.js
+++ scripts/script.js
@@ -6,6 +6,35 @@ function factorial(n) {
     };
   
     return n * factorial(n - 1);
-  };
+};
 
+///// Покращимо таку реалізацію, шляхом кешування обчислень ////
+
+function cachebleProcess(cbFactorial) {
+    const cache = {};
   
+    return function (...args) {
+      const key = args.join(',');
+  
+        if (key in cache) {
+          console.log(`Receive value from cache - ${cache[key]}`);
+          return cache[key];
+        };
+  
+      const result = cbFactorial(...args);
+      console.log(`Calculate -  ${result}`);
+      cache[key] = result;
+  
+      return result;
+    };
+};
+
+const memoFactorial = cachebleProcess(factorial);
+
+console.log(memoFactorial(3));
+
+console.log(memoFactorial(3));
+
+console.log(memoFactorial(10));
+
+console.log(memoFactorial(10));
