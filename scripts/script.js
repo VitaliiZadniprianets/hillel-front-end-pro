a3383849755ffe8a042545b0a7cb571acabd1881
Vitalii Zadniprianets <vzadnepr@ukr.net>
2023-04-29 13:21:45 +0300

    end

--- scripts/script.js
+++ scripts/script.js
@@ -1,40 +1,91 @@
-///// Створюємо функцію факторіал ////
-
-function factorial(n) {
-    if (n <= 1) {
-      return 1;
-    };
-  
-    return n * factorial(n - 1);
+const history = {
+  records: [],
+  get templateRecords() {
+    const template = this.records.map(
+      (record) =>
+        '<li class="record">' + JSON.stringify(record, null, 50) + "</li>"
+    );
+    return (
+      '<ul class="records card card__shadow">' + template.join(" ") + "</ul>"
+    );
+  },
+  drawRecords() {
+    document.write(this.templateRecords);
+  },
 };
 
-///// Покращимо таку реалізацію, шляхом кешування обчислень ////
+const shape = {
+  dependencies: Object.seal({
+    left: 100,
+    right: 100,
+    top: 100,
+    bottom: 100,
+  }),
+  get perimeter() {
+    //------ Bug ---
 
-function cachebleProcess(cbFactorial) {
-    const cache = {};
-  
-    return function (...args) {
-      const key = args.join(',');
-  
-        if (key in cache) {
-          console.log(`Receive value from cache - ${cache[key]}`);
-          return cache[key];
-        };
-  
-      const result = cbFactorial(...args);
-      console.log(`Calculate -  ${result}`);
-      cache[key] = result;
-  
-      return result;
+    //  ------ Your resolve problem there -----
+
+    //  ------ Your resolve problem there -----
+
+    // there are maybe heavy calculations
+    const total = Object.values(this.dependencies).reduce(
+      (accm, value) => accm + value,
+      0
+    );
+
+    // side effect
+    history.records.push({
+      dependencies: this.dependencies,
+      perimeter: total,
+    });
+
+    return total;
+  },
+
+  set perimeter(perimeter) {
+    if (!Number.isInteger(perimeter) || perimeter < 400) {
+      return;
     };
+
+    const size = perimeter / 4;
+
+    this.dependencies = Object.seal({
+      left: size,
+      right: size,
+      top: size,
+      bottom: size,
+    });
+
+    // side effect
+
+    history.records.push({
+      dependencies: this.dependencies,
+      perimeter: perimeter,
+    });
+  },
 };
 
-const memoFactorial = cachebleProcess(factorial);
+// shape.dependencies.foo = NaN; // ignored because  - sealed
+// delete shape.dependencies.left; // ignored because  - sealed
 
-console.log(memoFactorial(3));
+// shape.perimeter = 500; // write -- call setter
 
-console.log(memoFactorial(3));
+// console.log(shape.perimeter, "shape.perimeter"); // read -- call getter
 
-console.log(memoFactorial(10));
+// shape.dependencies.bottom = 200;
 
-console.log(memoFactorial(10));
+/// unoptimized operations
+console.log(shape.perimeter, "shape.perimeter");
+console.log(shape.perimeter, "shape.perimeter");
+console.log(shape.perimeter, "shape.perimeter");
+
+// draw records on screen
+history.drawRecords();
+
+// compare -- modify and current dependencies
+console.log(shape.dependencies, "shape.dependencies");
+console.log(
+  history.records[history.records.length - 1].dependencies,
+  "records"
+);
