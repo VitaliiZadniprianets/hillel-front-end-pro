d08d3d1e873049902ee6bc9787a2e1875795ae00
Vitalii Zadniprianets <vzadnepr@ukr.net>
2023-06-05 18:12:00 +0300

    homework41

--- scripts/script.js
+++ scripts/script.js
@@ -12,4 +12,30 @@ class HTMLElement {
     render() {
       console.log("rendering from HTMLElement");
     }
+  };
+
+  class HTMLElementInput extends HTMLElement {
+
+  #typeInputAbout = "" 
+
+  constructor (type, ...args) {
+    super(...args); 
+    this.#typeInputAbout = type;
   }
+
+  get type () {
+  return this.#typeInputAbout
+  } 
+
+  someMethod(string) {
+    this.text = string || 'some default text';
+    console.log("This is a method from HTMLElementInput");
+  }
+}
+
+
+const someInput = new HTMLElementInput ( "sometext", "input" , "inputclass", 17);
+
+someInput.someMethod("Hello Input");
+
+console.log(someInput);
