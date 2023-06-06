40aa0d82aff511eef524401b7ec7284263691212
Vitalii Zadniprianets <vzadnepr@ukr.net>
2023-06-06 14:03:58 +0300

    end

--- scripts/script.js
+++ scripts/script.js
@@ -1,41 +1,59 @@
 class HTMLElement {
-    constructor (tagName, className, id) {
-      this.tagName = tagName;
-      this.className = className;
-      this.id = id;
-    }
-  
-    rotate() {
-      console.log("rotating from HTMLElement");
-    }
-  
-    render() {
-      console.log("rendering from HTMLElement");
-    }
-  };
-
-  class HTMLElementInput extends HTMLElement {
-
-  #typeInputAbout = "" 
-
-  constructor (type, ...args) {
-    super(...args); 
-    this.#typeInputAbout = type;
+  constructor(tagName, className, id) {
+    this.tagName = tagName;
+    this.className = className;
+    this.id = id;
   }
 
-  get type () {
-  return this.#typeInputAbout
-  } 
+  rotate() {
+    console.log("rotating from HTMLElement");
+  }
 
-  someMethod(string) {
-    this.text = string || 'some default text';
-    console.log("This is a method from HTMLElementInput");
+  render() {
+    console.log("rendering from HTMLElement");
   }
 }
 
+class HTMLElementInput extends HTMLElement {
+  #typeInputAbout = "";
+  #value = "";
+  valid = false;
 
-const someInput = new HTMLElementInput ( "sometext", "input" , "inputclass", 17);
+  constructor(type, value, ...args) {
+    super(...args);
+    this.#typeInputAbout = type;
+    this.value = value;
+  }
+
+  get type() {
+    return this.#typeInputAbout;
+  }
+
+  get value() {
+    return this.#value;
+  }
+
+  set value(value) {
+    this.#value = value.trim();
+  }
+
+  someMethod(string) {
+    this.text = string || "some default text";
+    console.log("This is a method from HTMLElementInput");
+  }
+
+  validate() {
+    if (this.#value) {
+      this.valid = true;
+    } else {
+      this.valid = false;
+    }
+  }
+}
+
+let someInput = new HTMLElementInput("sometext", "Іnput", "myinput", "input class", 17);
 
 someInput.someMethod("Hello Input");
-
-console.log(someInput);
+someInput.value = "" ;
+someInput.validate();
+console.log(someInput,someInput.valid);
