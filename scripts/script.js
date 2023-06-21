f35567b63f0b93c65b772a390e24e0f49a9e6952
VitaliiZadniprianets <vzadnepr@ukr.net>
2023-06-21 19:57:38 +0300

    Homework48

--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,23 @@
 window.onload = function() {
-    let number = 1;
-  
-    function createCell() {
-      const cell = document.createElement("td");
-      cell.textContent = number++;
-      return cell;
-    };
-  
-    function createRow() {
-      const row = document.createElement("tr");
-  
-      for (let j = 1; j <= 10; j++) {
-        const cell = createCell();
-        row.append(cell);
-      };
-  
-      return row;
-    };
-  
-    function createTable() {
-      const table = document.createElement("table");
-  
-      for (let i = 1; i <= 10; i++) {
-        const row = createRow();
-        table.append(row);
-      };
-  
-      return table;
-    };
-  
-    setTimeout(function() {
-      const table = createTable();
-      document.body.append(table);
-    }, 3000);
+  const images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg'];
+
+  const getRandomImage = () => {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    return images[randomIndex];
+  };
+
+  const body = document.querySelector('.body');
+
+  const image = document.createElement('img');
+
+  const changeImage = () => {
+    const randomImage = getRandomImage();
+    image.src = `./images/${randomImage}`;
+  };
+
+  changeImage(); 
+
+  body.prepend(image);
+
+  setInterval(changeImage, 2000); 
 };
