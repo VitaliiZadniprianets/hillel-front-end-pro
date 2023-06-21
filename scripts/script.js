6b765c5c7243293579f59848ff0d12e7677e289c
VitaliiZadniprianets <vzadnepr@ukr.net>
2023-06-21 13:52:24 +0300

    Homework47

--- scripts/script.js
+++ scripts/script.js
@@ -1,64 +1,36 @@
 window.onload = function() {
-  const range = document.querySelector('.rangeSlider');
-  const numberInput = document.querySelector('.inputNumber');
-  const rangeDiagramm = document.querySelector('.range_diagramm');
-  const commissionDiagram = document.querySelector('.diagramm');
-  const resultTags = [...document.querySelectorAll('.results span')];
-  const commissionValue = {
-      '2%': {from: 0, to: 19},
-      '4%': {from: 20, to: 49},
-      '6%': {from: 50, to: 74},
-      '8%': {from: 75, to: 100},
-  };
-
-  range.addEventListener("input", onRangeInputHandler);
-    numberInput.addEventListener("input", onNumberInputHandler);
-
-
-    function onRangeInputHandler() {
-        numberInput.value = this.value;
-        modifyDiagram(this.value);
+    let number = 1;
+  
+    function createCell() {
+      const cell = document.createElement("td");
+      cell.textContent = number++;
+      return cell;
     };
-
-    function onNumberInputHandler() {
-        let value = this.value;
-
-        if(!this.checkValidity()) {
-            value = 0;
-        };
-
-        range.value = value;
-        modifyDiagram(value);
+  
+    function createRow() {
+      const row = document.createElement("tr");
+  
+      for (let j = 1; j <= 10; j++) {
+        const cell = createCell();
+        row.append(cell);
+      };
+  
+      return row;
     };
-
-    function modifyDiagram(value) {
-        rangeDiagramm.style.height = `${value}px`;
-        commissionDiagram.style.height = `${calculateCommission(commissionValue, value)}px`;
-        showResult(resultTags, rangeDiagramm.style.height, commissionDiagram.style.height);
+  
+    function createTable() {
+      const table = document.createElement("table");
+  
+      for (let i = 1; i <= 10; i++) {
+        const row = createRow();
+        table.append(row);
+      };
+  
+      return table;
     };
-
-    function calculateCommission(commissionObj, inputValue) {
-        for(let key in commissionObj) {
-            if (inputValue >= commissionObj[key].from && inputValue <= commissionObj[key].to) {
-                return toCoef(parseInt(key)) * inputValue;
-            };
-        };
-    };
-
-    function toCoef(persantage) {
-        return persantage / 100;
-    };
-
-    function showResult(tags, ...arg) {
-        const totalResult = arg.reduce((total, part) => {
-            return total + parseFloat(part);
-        }, 0);
-
-        arg.push(`${totalResult}px`);
-        let indexOfArg = 0;
-
-        tags.forEach(tag => {
-            tag.innerHTML = `${arg[indexOfArg++]}`;
-        });
-    };
+  
+    setTimeout(function() {
+      const table = createTable();
+      document.body.append(table);
+    }, 3000);
 };
