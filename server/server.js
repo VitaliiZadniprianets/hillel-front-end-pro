3d11b1e2b61121d193c7326ba55caada41351918
VitaliiZadniprianets <vzadnepr@ukr.net>
2023-07-03 17:34:48 +0300

    end

--- server/server.js
+++ server/server.js
@@ -2,4 +2,27 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-console.log("hello Everybody");
+app.get('/products', (req, res) => {
+  const products = [
+    {
+      id: 1,
+      name: 'Product 1',
+      price: 50
+    },
+
+    {
+      id: 2,
+      name: 'Product 2',
+      price: 100
+    }
+  ];
+
+  res.json(products);
+});
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
