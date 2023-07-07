d00a94aee478a6609926eec7f042533e4a6f1b2b
VitaliiZadniprianets <vzadnepr@ukr.net>
2023-07-07 15:11:10 +0300

    work

--- server/server.js
+++ server/server.js
@@ -1,21 +1,10 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const data = require('./data.json');
 
 app.get('/products', (req, res) => {
-  const products = [
-    {
-      id: 1,
-      name: 'Product 1',
-      price: 50
-    },
-
-    {
-      id: 2,
-      name: 'Product 2',
-      price: 100
-    }
-  ];
+  
 
   res.json(products);
 });
