9435cef2eb045bc46f8bfc03341309e51539e94f
VitaliiZadniprianets <vzadnepr@ukr.net>
2023-08-08 17:02:22 +0300

    hw 57

--- client-app/App.js
+++ client-app/App.js
@@ -1,41 +1,31 @@
 import Component from "@/plugins/component";
 import Loader from "@/common/components/loader/Loader";
+import { AsFragment } from "@/common/decorators";
 import Alert from "@/common/components/alert/Alert";
-import { AsNode } from "@/common/decorators";
 
 export default class App extends Component {
-  @AsNode
-  getTemplate() {
-    return `
+    @AsFragment
+    getTemplate() {
+        return `
             <main class="main-app d-flex min-vh-100">
                <div class="router-view d-flex flex-column"></div>
                <slot name="loader"></slot>
                <slot name="alert"></slot>
             </main>
-        `;
-  }
+        `
+    }
 
-  updateTemplate(template) {
-    return this.replaceSlot(
-      template,
-      {
-        key: 'slot[name="loader"]',
-        replacer: () => new Loader().render(),
-      },
-      {
-        key: 'slot[name="alert"]',
-        replacer: () =>
-          new Alert({
-            time: 3000,
-            maxWidth: 300,
-            close: true,
-    
-          }).render(),
-      },
-    );
-  }
+    updateTemplate(template) {
+        return this.replaceSlot(
+            template,
+            { key: 'slot[name="loader"]', replacer: () => new Loader().render() },
+            { key: 'slot[name="alert"]', replacer: () => new Alert().render() },
+        );
+    }
 
-  render() {
-    return this.updateTemplate(this.getTemplate());
-  }
+    render() {
+        return this.updateTemplate(
+            this.getTemplate()
+        )
+    }
 }
